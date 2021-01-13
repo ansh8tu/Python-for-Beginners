ba6dd4c8b8b734dece74ecb022776fbea3a083d2
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2021-01-13 17:18:27 +0530

    Added program for strings

---
+++ python101/4_typeConversion.py
@@ -0,0 +1,9 @@
+heardSongs = input('How many songs have you heard from the Without you Album by Lauv : ')
+print(type(heardSongs))
+
+totalSongs = 4
+print(type(totalSongs))
+
+unheardSongs = totalSongs - int(heardSongs)
+print('The number of Unheard songs are ' + str(unheardSongs))
+
