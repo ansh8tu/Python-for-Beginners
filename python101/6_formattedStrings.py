86a0e8bc38cbb13251aa67e13a99e066d19d9ee4
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2021-01-13 17:19:15 +0530

    Added program for formatted strings

---
+++ python101/6_formattedStrings.py
@@ -0,0 +1,7 @@
+artistName = "Lauv"
+songName = "Modern Loneliness"
+
+#in order to format your string you can use f it will allow you to format the string wothout concatination 
+#{} are holes which replace the content of the variables
+currentSong = f'Currently playing {songName} by {artistName} '
+print(currentSong)
