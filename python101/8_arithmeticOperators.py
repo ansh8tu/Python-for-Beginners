ab48189fb60fd85d0eebb9be1fe174ef408c86e9
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2021-01-14 19:42:48 +0530

    Added program for arithmetic operators

---
+++ python101/8_arithmeticOperators.py
@@ -0,0 +1,20 @@
+#returns sum
+print(10 + 3)
+
+#returns difference
+print(10 - 3)
+
+#returns product
+print(10*3)
+
+#returs floating type answer
+print(10/3)
+
+#returns integer type answer
+print(10//3)
+
+#returns remainder
+print(10%3)
+
+#returns exponent
+print(10**3)
