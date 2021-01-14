6e6a386d5ea0242a14aa6cc0d48235360c1bc36c
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2021-01-14 19:43:35 +0530

    Added program for operator precedence

---
+++ python101/9_operatorPrecedence.py
@@ -0,0 +1,11 @@
+#parenthesis has the highest precedence
+
+#followed by exponent
+
+#followed by multiplication or division
+
+#followed by addition or subtraction
+
+var = (10 + 3)**2 + 5 -3//2
+print(var)
+#prints 173
