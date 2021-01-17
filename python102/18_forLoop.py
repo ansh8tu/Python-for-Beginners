5d74717aa82417612b65bf1e9c664b6bf475f81d
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2021-01-18 00:33:04 +0530

    Added program for a for loop

---
+++ python102/18_forLoop.py
@@ -0,0 +1,11 @@
+costOfItems = [10, 20, 30]
+totalCost  = 0
+
+for item in costOfItems:
+    totalCost +=item
+    
+print(f'Total cost : {totalCost}')
+
+#to print numbers in a range we use range
+for i in range(10):
+    print(i)
