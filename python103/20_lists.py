fcae35b590308c6e7504c299bd2bbb888b6d714e
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2021-01-19 11:06:12 +0530

    Added program for lists

---
+++ python103/20_lists.py
@@ -0,0 +1,16 @@
+lauvSongs = ['Without you', 'Modern Loneliness', 'Dishes', 'Sims']
+
+#prints complete list as it is
+print(lauvSongs)
+
+#prints the first element in list 
+print(lauvSongs[0])
+
+#print the complete list
+print(lauvSongs[:])
+
+#prints the element from index 2 till the end
+print(lauvSongs[2:])
+
+lauvSongs[0]='Sweatpants'
+print(lauvSongs)
