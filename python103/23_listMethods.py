4a4153c88adc08eb9972647fda7606d602309323
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2021-01-20 12:23:44 +0530

    Added program for list methods

---
+++ python103/23_listMethods.py
@@ -0,0 +1,20 @@
+numbers =[10, 20, 5, 10, 2, 3]
+
+#to add numbers at the end
+numbers.append(20)
+print(numbers)
+
+#to insert element at a particular index
+numbers.insert(1, 15)
+print(numbers)
+
+#to check if number in list or not
+print(50 in numbers)
+
+#to remove a particular number from list
+numbers.remove(10)
+print(numbers)
+
+#to clear the whole list
+numbers.clear()
+print(numbers)
