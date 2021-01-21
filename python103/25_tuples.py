5a52108cd2a00883fdbf574099f66e5098212360
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2021-01-21 10:45:23 +0530

    Added program for tuples

---
+++ python103/25_tuples.py
@@ -0,0 +1,8 @@
+#tuples are similar to that of lists but the difference is that tuples are immutable
+#the word immutable means that you can't add ort remove items from a tuple 
+#these are useful when you don't want to make changes in the list
+
+numbers = (1, 2, 3)
+moreNumbers =(4,5,6)
+idk =numbers.__add__(moreNumbers)
+print(idk)
