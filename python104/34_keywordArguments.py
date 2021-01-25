d835528621c2ffbeb4d0fe7f0dd109250909f8d5
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2021-01-25 23:12:35 +0530

    Added program for keyword arguments

---
+++ python104/34_keywordArguments.py
@@ -0,0 +1,6 @@
+def addSongs(song, artist):
+    print(f'Added song {song}, by {artist}!')
+
+
+#always use keyword arguments at last else you'll get an error 
+addSongs(song = "Vibez", artist = "Zayn" )
