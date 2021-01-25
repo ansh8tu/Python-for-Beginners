94ce2691a264ebad348dda021f0d774b3e4bc45a
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2021-01-26 01:14:44 +0530

    Added program for return keyword

---
+++ python104/35_returnKeyword.py
@@ -0,0 +1,7 @@
+def areaOfRectangle(length, breadth):
+    return length * breadth
+
+area = areaOfRectangle(length = 10, breadth = 20)
+print(f'The area of rectangle is {area}')
+
+#by default if no return statement is provided then it returns None
