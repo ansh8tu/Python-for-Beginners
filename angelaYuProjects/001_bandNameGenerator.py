b316794cd977edbe8f2d8d83cee79dc1a55fb0a6
ansh8tu <anshu8tu@gmail.com>
2021-06-17 17:01:41 +0530

    Added project band name generator!

---
+++ angelaYuProjects/001_bandNameGenerator.py
@@ -0,0 +1,6 @@
+print("Hello, Welcome to the Band Name Generator!")
+
+cityName = input("Enter the name of your city! \n")
+petName = input("Enter your pet name!\n")
+
+print(f"Your band's name is {cityName} {petName}!")
